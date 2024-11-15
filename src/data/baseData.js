aa2a04470e54ecb49e4a20fb00b5fb6bce01333c
Sam Almohanna <webdevsumayah@gmail.com>
2024-11-14 22:29:13 -0500

    refactor: scene managment and transitions.

---
+++ src/data/baseData.js
@@ -0,0 +1,9 @@
+export default {
+	title: `Grandpa's Secret`,
+	description: `A point & click game about grandpa's secret. Made during Github Game Off 2024`,
+	url: `https://grandpas-secret.vercel.app`, // No trailing slash!
+	// JSON LD
+	name: `Grandpa's Secret`,
+	//OG Image
+	ogImage: '/og-image.png',
+}
